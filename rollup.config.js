9aac38b1ad28be723a17a73b61264cf297e17d45
tjk5903 <tjk5903@psu.edu>
2024-04-14 11:00:31 -0400

    rollup again

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
 export default {
-  input: 'src/index.js', // Change 'src/index.js' to the entry point of your application
+  input: 'index.html', // Change 'src/index.js' to the entry point of your application
   output: {
     file: 'dist/bundle.js', // Change 'dist/bundle.js' to the desired output bundle file
     format: 'es', // Specify the output format (ES module in this case)
